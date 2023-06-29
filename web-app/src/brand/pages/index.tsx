fd75efa817c0f23e7769889335f532acbf44ea8b
toannt97 <nguyenthaitoan1997@gmail.com>
2023-06-30 00:46:27 +0700

    feat: add a new plugin:  viewport extra

--- web-app/src/brand/pages/index.tsx
+++ web-app/src/brand/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import 'viewport-extra';
 import '../../design/styles/index.scss';
 import App from '../routes';
 import { store } from '../store';
