84cd489a3ac7e4a1a97fda89f0caeb1aa7e6eaf9
toan.nguyen <toan.nguyen@4digit.jp>
2023-07-20 17:47:50 +0700

    fix: adjust logic of component theme switcher

--- web-app/src/design/components/theme-switcher/index.tsx
+++ web-app/src/design/components/theme-switcher/index.tsx
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { loadFromLocalStorage, saveToLocalStorage } from '../../../brand/utils/utils';
 import { Icon } from '../../components/icon';
 import { mapModifiers } from '../../libs/components';
 
 export const ThemeSwitcher: React.FC = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
-  const currentThemeSetting = loadFromLocalStorage('theme');
-  const theme = currentThemeSetting ?? 'light';
+
+  useEffect(() => {
+    const currentThemeSetting = loadFromLocalStorage('theme');
+    const theme = currentThemeSetting ?? 'light';
+
+    document.documentElement.setAttribute('data-theme', theme);
+    setIsDarkMode(theme === 'dark');
+  }, []);
 
   const handleChangeTheme = () => {
-    const newTheme = isDarkMode ? 'dark' : 'light';
+    const newTheme = isDarkMode ? 'light' : 'dark';
 
     saveToLocalStorage('theme', newTheme);
-    document.documentElement.setAttribute('data-theme', newTheme);
     setIsDarkMode(!isDarkMode);
   };
 
   return (
-    <div className={mapModifiers('c-theme-switcher', theme)} onClick={handleChangeTheme}>
+    <div className={mapModifiers('c-theme-switcher', isDarkMode && 'dark')} onClick={handleChangeTheme}>
       <span className="c-theme-switcher__indicator" />
       <span className={mapModifiers('c-theme-switcher__icon', 'light-mode')}>
         <Icon name="sun" />
